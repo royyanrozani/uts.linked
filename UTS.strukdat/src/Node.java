66934afd3214ce5e94e47dd9d776a38098d79461
royyanrozani <54833906+royyanrozani@users.noreply.github.com>
2020-08-31 23:51:41 +0700

    Add files via upload

---
+++ UTS.strukdat/src/Node.java
@@ -0,0 +1,22 @@
+//m.royyan rozani
+//402019611023
+
+public class Node {
+    private String angka;
+    private Node nextNode;
+
+    public String getAngka() {
+        return angka;
+    }
+
+    public void setAngka(String angka) {
+        this.angka = angka;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
